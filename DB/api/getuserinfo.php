34d7541ffacbf766805e657586fce7aff295aa69
estevetarra <ets1995@hotmail.com>
2016-02-20 00:47:33 +0100

    Making something

---
+++ DB/api/getuserinfo.php
@@ -0,0 +1,16 @@
+<?php
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+
+echo json_encode($arr);
+
+echo "2";
+echo EOL;
+echo "JoanMaria";
+echo EOL;
+echo "100";
+echo EOL;
+echo "Location";
+echo EOL;
+echo "4";
+
+?>
