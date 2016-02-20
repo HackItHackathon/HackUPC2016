c3883a7d43f02c5752fdfa1058a0991002ebbcce
mariavila <mariava.1213@gmail.com>
2016-02-20 21:45:54 +0100

    moltes coses

---
+++ Unity/Assets/PointController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PointController : MonoBehaviour {
+    public Text txt;
+    private int currentscore = 0;
+
+	// Use this for initialization
+	void Start () {
+        string url = "http://interact.siliconpeople.net/hackathon/getuserinfo?id=" + GameController.instance.ID;
+        WWW web = new WWW(url);
+        while (!web.isDone) ;
+        Getuserinfo user = new Getuserinfo();
+        Debug.Log(web.text);
+
+        JsonUtility.FromJsonOverwrite(web.text, user);
+        currentscore = user.punt;
+        txt.text = currentscore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(txt!=null) txt.text = currentscore.ToString();
+
+	}
+}
