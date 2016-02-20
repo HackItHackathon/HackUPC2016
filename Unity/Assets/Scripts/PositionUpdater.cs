7bdc5afcaedc9f7a1b89f5d7a25ac3bbe27c6ac7
jmigual <j.marce.igual@gmail.com>
2016-02-20 03:28:19 +0100

    Added position updater and created material

---
+++ Unity/Assets/Scripts/PositionUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionUpdater : MonoBehaviour {
+
+    public Vector2[] positions = { new Vector2(2f, 3f), new Vector2(-1f, 4f) };
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
