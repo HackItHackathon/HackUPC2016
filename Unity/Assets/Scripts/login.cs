f4f63503b8cc5fd705e9341da86a08b55dda89a2
estevetarra <ets1995@hotmail.com>
2016-02-20 13:42:43 +0100

    loggin start

---
+++ Unity/Assets/Scripts/login.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Awake () {
+	    if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    public static void onClick()
+    {
+
+    }
+	
+}
