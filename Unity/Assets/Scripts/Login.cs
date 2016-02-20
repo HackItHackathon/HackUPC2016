0e3943cdc15476722619a4c9ff1a476618020ef9
jmigual <j.marce.igual@gmail.com>
2016-02-20 18:09:13 +0100

    Changed size

---
+++ Unity/Assets/Scripts/Login.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+ 
+
+/*
+public class Getuserid
+{
+    public int id;
+}*/
+
+public class Login : MonoBehaviour {
+
+    public Text userName;
+
+	// Use this for initialization
+	void Awake () {
+
+    }
+
+    public void onClick()
+    {
+        Debug.Log(userName.text);
+        string url = "http://interact.siliconpeople.net/hackathon/getuserid?nom=" + userName.text;
+        WWW web = new WWW(url);
+        while (!web.isDone) ;
+        Debug.Log(web.text);
+        Getuserid myObject = new Getuserid();
+        JsonUtility.FromJsonOverwrite(web.text, myObject);
+        Debug.Log(myObject.id);
+        GameController.instance.ID = myObject.id;
+        SceneManager.LoadScene("menu");
+    }
+
+}
