6c6b39819d90491861e6ff71c0e8e0f51c367e51
jmigual <j.marce.igual@gmail.com>
2016-02-20 01:40:33 +0100

    Added boundary and some backgrounds

---
+++ Unity/Assets/Scripts/Boundary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour
+{
+    public static float minX = -7.5f;
+    public static float maxX = 7.5f;
+    public static float minY = -10f;
+    public static float maxY = 10;
+}
