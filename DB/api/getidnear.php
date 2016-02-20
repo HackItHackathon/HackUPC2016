1898f98224bce19b7f5ef253145ed8a599d35eb7
estevetarra <ets1995@hotmail.com>
2016-02-20 12:08:58 +0100

    first operative version of getidnear

--- DB/api/getidnear.php
+++ DB/api/getidnear.php
@@ -1,6 +1,23 @@
 <?php
 
-//echo $_GET['id'];
+class user {
+	public $distance;
+	public $id;
+	public $nom;
+	public $latitude;
+	public $longitude;
+	public $punt;
+}
+
+function calcdistance ($la1,$lo1,$la2,$lo2){
+	$R = 6371000; // metres
+	$ila = ($la2-$la1);
+	$ilo = ($lo2-$lo1);
+	$a = sin($ila/2) * sin($ila/2) + cos($la1) * cos($la2) * sin($ilo/2) * sin($ilo/2);
+	$c = 2 * atan2(sqrt($a), sqrt(1-$a));
+	$d = $R * $c;
+	return $d;
+}
 
 $servername = "localhost";
 $username = "hackathon";
@@ -18,18 +35,41 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, nom, latitude,longitude, punt FROM users WHERE id = '" . $_GET['id'] . "'";
 
+//echo $sql;
+
 $result = $conn->query($sql);
 
 $row= $result->fetch_row();
 
-$arr = array('id' => $row[0], 'nom' => $row[1],'punt' => $row[5],
-		'location' => array('latitude' => $row[2], 'longitude' => $row[3]), 'ida' => $row[4]);
+$user = new user();
+$user->distance = 0;
+$user->id = $row['id'];
+$user->nom = $row['nom'];
+$user->latitude = $row['latitude'];
+$user->longitude = $row['longitude'];
+$user->punt = $row['punt'];
 
-echo json_encode($arr);
+$arrayusers = array();
 
+$sql = "SELECT id, nom, latitude,longitude, punt FROM users WHERE id != '" 
+		. $_GET['id'] . "'" . " and ida != '" . $_GET['id'] . "'";
 
-$arr = array('id' => array(1,2,3));
+//echo $sql;
 
-echo json_encode($arr);
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+	$userf = new user();
+	$userf->id = $row['id'];
+	$userf->nom = $row['nom'];
+	$userf->latitude = $row['latitude'];
+	$userf->longitude = $row['longitude'];
+	$userf->punt = $row['punt'];
+	$userf->distance = calcdistance($user->latitude, $user->longitude, $userf->latitude, $userf-> longitude);
+	array_push($arrayusers, $userf);
+}
+asort($arrayusers);
+
+echo json_encode($arrayusers);
 
 ?>
